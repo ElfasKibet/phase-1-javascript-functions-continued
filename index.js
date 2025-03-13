2ba064dd50ff13452e10c8dec373d01b53cf939f
elfas <kibetelfas17@gmail.com>
2025-03-13 22:03:04 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(game = "roller-skate") {
+  return `This Saturday, I want to ${game}!`;
+}
+console.log(saturdayFun());
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+  return function (adj = "special") {
+    return `You are ${flair}${adj}${flair}!`;
+  };
+}
+console.log(wrapAdjective()("a hard worker"));
